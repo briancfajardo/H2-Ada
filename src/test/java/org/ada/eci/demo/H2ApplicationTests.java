8193f6a58b37321780ec4bdf28913ce66f8e9f02
Brian Camilo Fajardo Sánchez <briancfajardo@gmail.com>
2024-02-14 19:18:47 -0500

    h2 DB

---
+++ src/test/java/org/ada/eci/demo/H2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.eci.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
